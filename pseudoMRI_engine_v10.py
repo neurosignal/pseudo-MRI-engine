5b78f05ec405e822c5b88de4cd0973790c05a8e6
Amit Jaiswal <amitjaiswal.elect@gmail.com>
2024-10-25 02:38:33 +0300

    pseudoMRI_engine version 1.0 main script.

---
+++ pseudoMRI_engine_v10.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 09:59:05 2022
+
+@author: Amit Jaiswal(1,2)
+1.MEGIN OY, Espoo, Finland 
+2.NBE, Aalto University, Espoo, Finland
+Contact: amit.jaiswal@megin.fi OR amit.jaiswal@aalto.fi
+
+Using pseudo-MRI engine version 1.0
+Jaiswal, A., Nenonen, J., & Parkkonen, L., Pseudo-MRI engine for MRI-free
+electromagnetic source imaging. Submitted to Human Brain Mapping in Apr.
+2024.
+pseudo-MRI engine is a research tool intended for generating a FreeSurfer-segmented 
+set of subject-spectific MRI template. 
+The user can use it under a collaboration agreement with Megin/developer, but WITHOUT ANY WARRANTY.
+Using or sharing it fully or partially beyond the agreement MUST BE agreed with Megin/developer in prior.
+The users should use the tool at their own responsibility.
+version notes:
+    * first stable version
+"""
+print(__doc__)
+import nimi; del nimi
+#%% 1. Set subject and directories
+import argparse
+parser = argparse.ArgumentParser(description='pseudo-MRI engine')
+parser.add_argument('-s',       '--subject',        default=None,  type=str,  help='subject name')
+parser.add_argument('-s',       '--template',       default=None,  type=str,  help='Template MRI name')
+parser.add_argument('-s',       '--template_dir',   default=None,  type=str,  help='Template MRI parent directory')
+parser.add_argument('-dig',     '--isotrakfile',    default=None,  type=str,  help='MEG file with isotrak information (default=None)')
+parser.add_argument('-trans',   '--trans_file',     default=None,  type=str,  help='Co-registration (sRAS-to-head) file (default=None)')
+parser.add_argument('--dense_hsp',          action='store_true',      help='densify HSP?')
+parser.add_argument('--show_good_hsps_idx', action='store_true',      help='show good hsps idices?')
+parser.add_argument('-v', '--verbose',      action='store_true',      help='verbose mode or not?')
+parser.add_argument('--nmax_Ctrl',          default=200,    type=int, help='Number of maximum control points.')
+args = parser.parse_args()
+
+# Load modules
+from mne.surface import read_surface
+import os
+import meginpy
+n_jobs = os.cpu_count()
+print("\nhostname \t= %s \n#cores \t\t= %d" % (os.uname().nodename, n_jobs))
+meginpy.utils.tic()
+
+#%% Generate pseudo MRI
+templ_surf_fname  = '%s/%s/bem/watershed/%s_outer_skin_surface'%(args.templates_dir, args.template, args.template)
+template_headsurf = read_surface(templ_surf_fname, return_dict=True)[2]
+template_headsurf['rr'] /= 1000  # covert to meter    
+template_headsurf['rr'] *= 0.975 # shrink inward to reduce mismatch (FIX it by checking the mean distance for points above fids) 
+
+args.use_hpi             = True
+args.show_good_hsps_idx  = False
+args.rem_good_pts_idx    = []
+args.which_mri_to_warp   = ['T1.mgz', 'brain.mgz'][:1] # 'all'
+
+#%% Run PseudoMRIGenerator_ver1
+meginpy.pseudoMRI.PseudoMRIGenerator_ver1(args.isotrak_file, args.template, args.templates_dir, args.fiducial_file, args.pseudo_subject, 
+                                          args.pseudo_subjects_dir, isotrak_from=args.subject, dense_hsp=False, mirror_hsps=True, 
+                                          template_headsurf=template_headsurf, dense_surf=False, z_thres=0.02, 
+                                          destHSPsShiftInwrd=0.0015, Wreg_est=1e-10, Wreg_apply= 1e-10, wtol=1e-6,
+                                          warp_anatomy=True, which_mri=args.which_mri_to_warp, blocksize=500000, 
+                                          write2format=['.mgz'], 
+                                          n_jobs=n_jobs, toplot=True, toooplot=False, pyplot_fsize=12, 
+                                          save_pseudomri_plot=True, plot_zoom_in='10%', plot_nslice=16, plot_tol=3, 
+                                          plot_side_leave='25%', plot_lw=1.5, plot_titlecolor=(.8,.9,.2), plot_titlefsize=18, 
+                                          isotrak_reject_min_max=[2, 10], use_hpi=args.use_hpi, 
+                                          show_good_hsps_idx=args.show_good_hsps_idx, rem_good_pts_idx=args.rem_good_pts_idx,
+                                          nmax_Ctrl=args.nmax_Ctrl)
+meginpy.utils.toc()
